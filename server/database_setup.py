5bb89d338721936ad8326616bc0d8b29d1636e51
SRshohan <srshohan02@gmail.com>
2024-08-01 14:47:01 -0400

    Changed on the api keys setup

--- server/database_setup.py
+++ server/database_setup.py
@@ -1,16 +1,31 @@
 from pyrebase import pyrebase
+from dotenv import load_dotenv
+import os
+
+
+file_path = ".env.dev"
+
+load_dotenv(f"server/{file_path}")
+apikey = os.getenv("API_KEY")
+authDomain = os.getenv("AUTH_DOMAIN")
+databaseURL = os.getenv("DATABASE_URL")
+projectId = os.getenv("PROJECT_ID")
+storageBucket = os.getenv("STORAGE_BUCKET")
+messagingSenderId = os.getenv("MESSAGING_SENDER_ID")
+appId = os.getenv("APP_ID")
+measurementId = os.getenv("MEASUREMENT_ID")
 
 
 def database_authentication():
     firebaseConfig = {
-  "apiKey": "AIzaSyDX2DH0jiErkd2bk1uiGDNS1muLHdq-8d8",
-  "authDomain": "fitness-app-e3aa0.firebaseapp.com",
-  "databaseURL": "https://fitness-app-e3aa0-default-rtdb.firebaseio.com",
- "projectId": "fitness-app-e3aa0",
-  "storageBucket": "fitness-app-e3aa0.appspot.com",
-  "messagingSenderId": "93872114626",
-  "appId": "1:93872114626:web:bc89804869eb17440e645b",
-  "measurementId": "G-3R15VLJNY2"
+  "apiKey": f"{apikey}",
+  "authDomain": f"{authDomain}",
+  "databaseURL": f"{databaseURL}",
+ "projectId": f"{projectId}",
+  "storageBucket": f"{storageBucket}",
+  "messagingSenderId": f"{messagingSenderId}",
+  "appId": f"{appId}",
+  "measurementId": f"{measurementId}"
 }
 
 
